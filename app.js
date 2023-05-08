b8c596ce7a28e774732d32a6503349a48d0d77f5
KatarzynaBurdzy <katarzyna.anna.burdzy@gmail.com>
2023-05-08 13:27:04 +0200

    second commit

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const require = require("require");
+const request = require("request");
 const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
 
 app.listen(PORT, () => console.log(`Server up and running on port ${PORT}`));
